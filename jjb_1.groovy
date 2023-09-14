17ad556316c030c15ac021714364c725eadf6f0e
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-14 17:43:08 +0530

    Add files via upload

--- jjb_1.groovy
+++ jjb_1.groovy
@@ -1,55 +1,32 @@
-import jenkins.model.*
-import hudson.model.*
-import hudson.model.StringParameterDefinition
-import hudson.tasks.*
-import hudson.tasks.Shell
-import hudson.scm.NullSCM
-import hudson.util.*
+def env = envParam.get('env')
 
-def createJenkinsJob(environment) {
-    def jenkins = Jenkins.getInstance()
+def viewExists = Jenkins.instance.views.find { it.name == 'SampleCS_'+env }
 
-    // Check if the view already exists, and create it if not
-    def viewName = "MyView_${environment}"
-    def listView = jenkins.getView(viewName)
-    if (listView == null) {
-        listView = new ListView(viewName, jenkins)
-        listView.owner = jenkins
-        listView.save()
-    }
+if (!viewExists) {
+  def view = new View(name: 'SampleCS_'+env)
+  view.description = "Jenkins view for ${env}"
+  view.jobs = ['job1', 'job2']
 
-    // Create a FreeStyle job
-    def jobName = "MyJob_${environment}"
-    def job = new FreeStyleProject(jenkins, jobName)
-    job.save()
-
-    // Set log rotation
-    job.buildDiscarder = new LogRotator(4, -1, -1, -1)
-
-    // Add a string parameter
-    job.addProperty(new ParametersDefinitionProperty(
-        new StringParameterDefinition("custName", "NONE")
-    ))
-
-    // Enable concurrent builds
-    job.concurrentBuild = true
-
-    // Configure source code management (none)
-
-
-    // Add an Execute Shell build step
-    job.getBuildersList().add(new Shell("echo 'Hello, Jenkins! This is ${environment}'"))
-
-    job.save()
+  Jenkins.instance.addView(view)
 }
 
-// Call the function with the desired 'env' value
-// Replace with your desired environment
-def environment = env
-println("View '$environment' created.")
-createJenkinsJob(environment)
+def job = Jenkins.instance.getItemByFullName(env + '_job')
 
+if (!job) {
+  def jobConfig = """
+    logRotation: 4
+    stringParameterDefinitions:
+      - name: custName
+        value: NONE
+    concurrentBuilds: true
+    scm: none
+    axes:
+      - axisName: env
+        values: [${env}]
+      - axisName: userDefined
+        values: ['value1', 'value2']
+    shell: echo "This is a shell command"
+  """
 
-
-
-
+  job = Jenkins.instance.createItem(jobConfig, 'Freestyle project')
+}
