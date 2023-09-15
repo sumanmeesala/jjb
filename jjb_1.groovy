6465e2396b8ff8d050571cf72c84a3af487294cd
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-15 06:39:33 +0530

    Add files via upload

--- jjb_1.groovy
+++ jjb_1.groovy
@@ -1,60 +1,55 @@
-import hudson.model.*
-import hudson.tasks.*
-import hudson.util.Secret
+pipeline {
+    agent any
 
-def createMatrixJob(env) {
-    def jenkins = Jenkins.getInstance()
-
-    // Check if the view already exists, and create it if not
-    def viewName = "View_${env}"
-    def listView = jenkins.getView(viewName)
-    if (listView == null) {
-        listView = new ListView(viewName, jenkins)
-        listView.owner = jenkins
-        listView.save()
+    parameters {
+        string(name: 'env', description: 'Environment', defaultValue: 'dev')
     }
 
-    // Create a Freestyle Project inside the view
-    def jobName = "Job_${env}"
-    def job = new FreeStyleProject(jenkins, jobName)
-    job.save()
+    stages {
+        stage('Create Matrix-Like Job') {
+            steps {
+                script {
+                    def viewName = "View_${params.env}"
+                    def jobName = "Job_${params.env}"
 
-    // Set log rotation
-    job.buildDiscarder = new LogRotator(4, -1, -1, -1)
+                    // Check if the view already exists, and create it if not
+                    def listView = jenkins.getView(viewName)
+                    if (listView == null) {
+                        listView = new ListView(viewName, jenkins)
+                        listView.owner = jenkins
+                        listView.save()
+                    }
 
-    // Add a string parameter "custName" with default value "NONE"
-    job.addProperty(new ParametersDefinitionProperty(
-        new StringParameterDefinition("custName", "NONE")
-    ))
+                    // Create a Freestyle Project inside the view
+                    def job = Jenkins.instance.createProject(FreeStyleProject, jobName)
+                    job.save()
 
-    // Enable concurrent builds
-    job.concurrentBuild = true
+                    // Set log rotation
+                    job.buildDiscarder = new LogRotator(4, -1, -1, -1)
 
+                    // Add a string parameter "custName" with default value "NONE"
+                    job.addProperty(new ParametersDefinitionProperty(
+                        new StringParameterDefinition("custName", "NONE")
+                    ))
 
+                    // Enable concurrent builds
+                    job.concurrentBuild = true
 
-    // Add dynamic axis
-    job.getBuildersList().add(new Shell("echo 'Hello, Jenkins! This is $env'"))
+                    // Configure source code management (none)
+                    job.scm = new NullSCM()
 
-    // Add user-defined axis
-    job.getBuildersList().add(new Shell("echo 'User-defined Axis: AxisValues'"))
+                    // Add dynamic and user-defined axes (simulated with build steps)
+                    job.getBuildersList().add(new Shell("echo 'Hello, Jenkins! This is ${params.env}'"))
+                    job.getBuildersList().add(new Shell("echo 'User-defined Axis: AxisValues'"))
 
-    // Add masked and regex password parameters (replace with actual values)
-    def maskedPassword = Secret.fromString("your_masked_password")
-    def regexPassword = Secret.fromString("your_regex_password")
-    job.addProperty(new ParametersDefinitionProperty(
-        new PasswordParameterDefinition("maskedPassword", maskedPassword, "Masked Password"),
-        new PasswordParameterDefinition("regexPassword", regexPassword, "Regex Password")
-    ))
+                    // Save the job
+                    job.save()
 
-    // Save the job
-    job.save()
-
-    // Print a message
-    println("Jenkins Freestyle Matrix Job '$jobName' created inside the view '$viewName'.")
+                    // Print a message
+                    echo "Jenkins Freestyle Matrix-Like Job '$jobName' created inside the view '$viewName'."
+                }
+            }
+        }
+    }
 }
-
-// Call the function with the desired 'env' value
-// Replace with your desired environment
-def environment = env
-createMatrixJob(environment)
 
