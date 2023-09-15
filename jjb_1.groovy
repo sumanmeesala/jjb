a26e8b80057ab6895f844b6dd52333cab0cae157
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-15 07:09:48 +0530

    Add files via upload

--- jjb_1.groovy
+++ jjb_1.groovy
@@ -1,55 +1,72 @@
-pipeline {
-    agent any
+package hudson.cli;
 
-    parameters {
-        string(name: 'env', description: 'Environment', defaultValue: 'dev')
+import hudson.model.*
+import jenkins.model.Jenkins
+import hudson.model.ListView
+import jenkins.model.GlobalConfiguration
+import hudson.tasks.*
+import hudson.util.Secret
+
+
+    def jenkins = Jenkins.instance
+    def viewName = '${env}_CS_view'
+    // Check if the view already exists, and create it if not
+    def existingName = jenkins.getView(viewName)
+    
+    if (existingName == null) {
+        def newView = new ListView(viewName)
+        jenkins.addview(newView)
+        println("View '$viewName' created.")
+        } else {
+        println("View '$viewName' already exists.")
     }
 
-    stages {
-        stage('Create Matrix-Like Job') {
-            steps {
-                script {
-                    def viewName = "View_${params.env}"
-                    def jobName = "Job_${params.env}"
-
-                    // Check if the view already exists, and create it if not
-                    def listView = jenkins.getView(viewName)
-                    if (listView == null) {
-                        listView = new ListView(viewName, jenkins)
-                        listView.owner = jenkins
-                        listView.save()
-                    }
-
-                    // Create a Freestyle Project inside the view
-                    def job = Jenkins.instance.createProject(FreeStyleProject, jobName)
-                    job.save()
-
-                    // Set log rotation
-                    job.buildDiscarder = new LogRotator(4, -1, -1, -1)
-
-                    // Add a string parameter "custName" with default value "NONE"
-                    job.addProperty(new ParametersDefinitionProperty(
-                        new StringParameterDefinition("custName", "NONE")
-                    ))
-
-                    // Enable concurrent builds
-                    job.concurrentBuild = true
-
-                    // Configure source code management (none)
-                    job.scm = new NullSCM()
-
-                    // Add dynamic and user-defined axes (simulated with build steps)
-                    job.getBuildersList().add(new Shell("echo 'Hello, Jenkins! This is ${params.env}'"))
-                    job.getBuildersList().add(new Shell("echo 'User-defined Axis: AxisValues'"))
-
-                    // Save the job
-                    job.save()
-
-                    // Print a message
-                    echo "Jenkins Freestyle Matrix-Like Job '$jobName' created inside the view '$viewName'."
-                }
-            }
+    matrixJob('${env}_CS_job') {
+    description('This is an 1st cc Job DSL job')
+    
+    configure { project ->
+         project / 'logRotator' {
+            numtokeep(4)
+                                }
+              }
+              
+    concurrentBuild(true)
+    
+    scm {
+      none()
         }
-    }
-}
+        
+    parameters {
+        stringParam('custName', 'NONE','description')
+               }
+               
+    axes {
+           axis {
+                  name ('dynaxis')
+                  valueString(DYNAMIC)
+                }
+         }
+         
+    def globalConfiguration = GlobalConfiguration.all().find {it.displayName == 'Mask Passwords and Regexes' }
+    
+    if (globalConfiguration) {
+          globalConfiguration.setMaskPasswords(true)
+          globalConfiguration.setMaskPasswordsConsoleLog(true)
+          globalConfiguration.save()
 
+          System.setProperty("org.jenkinsci.plugins.workflow.steps.SecretEnvVarHelper.CONFIGURED", "true")
+          
+          println("Enabled 'Mask passwords and regexes' feature and global passwords.")
+          } else {
+          println("'Mask passwords and regexes' feature and global passwords not found.")
+          }
+    
+    steps {
+      shell('echo ""')
+          }
+          
+myView = hudson.model.Hudson.instance.getView('${env}_CS_view')
+myView.doAddJobToView('${env}_CS_job')
+jenkins.save()
+
+}
