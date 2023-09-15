bc0df6dc3fcbefcf3df60f5c5a62cac32b46d21c
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-15 05:47:12 +0530

    Add files via upload

--- jjb_1.groovy
+++ jjb_1.groovy
@@ -1,51 +1,70 @@
-import jenkins.model.*
 import hudson.model.*
-import hudson.model.StringParameterDefinition
 import hudson.tasks.*
-import hudson.tasks.Shell
-import hudson.util.*
+import hudson.matrix.*
+import hudson.util.Secret
 
-def createMatrixJob(environment) {
+def createJenkinsJob(env) {
     def jenkins = Jenkins.getInstance()
-
+    
     // Check if the view already exists, and create it if not
-    def viewName = "MyView_${environment}"
-    def listView = jenkins.getView(viewName)
-    if (listView == null) {
-        listView = new ListView(viewName, jenkins)
-        listView.owner = jenkins
-        listView.save()
+    def viewName = "View_${env}"
+    def view = jenkins.getView(viewName)
+    if (view == null) {
+        view = new ListView(viewName, jenkins)
+        view.owner = jenkins
+        view.save()
     }
-
-    // Create a Matrix Job
-    def jobName = "MyMatrixJob_${environment}"
-    def job = new MatrixJob(jenkins, jobName)
+    
+    // Create a Matrix Project inside the view
+    def jobName = "Job_${env}"
+    def job = new MatrixProject(jenkins, jobName)
     job.save()
-
+    
     // Set log rotation
     job.buildDiscarder = new LogRotator(4, -1, -1, -1)
-
-    // Add a string parameter
+    
+    // Add a string parameter "custName" with default value "NONE"
     job.addProperty(new ParametersDefinitionProperty(
         new StringParameterDefinition("custName", "NONE")
     ))
-
+    
     // Enable concurrent builds
     job.concurrentBuild = true
-
+    
     // Configure source code management (none)
-
-    // Define the axes for the Matrix Job
-
-    // Add an Execute Shell build step
-    job.getBuildersList().add(new Shell("echo 'Hello, Jenkins! This is ${environment}'"))
-
+    job.scm = new NullSCM()
+    
+    // Add dynamic axis
+    job.getAxes().add(new Axis("DynamicAxisName", "DynamicAxisValues"))
+    
+    // Add user-defined axis
+    job.getAxes().add(new UserDefinedAxis("AxisName", "AxisValues"))
+    
+    // Add an Execute Shell build step with a sample Java command
+    def shellScript = """
+        #!/bin/bash
+        echo 'Hello, Jenkins! This is $env'
+        java -jar your-java-application.jar --param1 \$custName
+    """
+    job.getBuildersList().add(new Shell(shellScript))
+    
+    // Add masked and regex password parameters (replace with actual values)
+    def maskedPassword = Secret.fromString("your_masked_password")
+    def regexPassword = Secret.fromString("your_regex_password")
+    job.addProperty(new ParametersDefinitionProperty(
+        new PasswordParameterDefinition("maskedPassword", maskedPassword, "Masked Password"),
+        new PasswordParameterDefinition("regexPassword", regexPassword, "Regex Password")
+    ))
+    
+    // Save the job
     job.save()
+    
+    // Print a message
+    println("Jenkins Job '$jobName' created inside the view '$viewName'.")
 }
 
 // Call the function with the desired 'env' value
 // Replace with your desired environment
 def environment = env
-println("Matrix Job '$environment' created.")
-createMatrixJob(environment)
+createJenkinsJob(environment)
 
