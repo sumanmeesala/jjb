9147cdc7a5ecd443b698ca9ef3f692dcd6a36cb6
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-15 07:14:58 +0530

    Add files via upload

--- jjb_1.groovy
+++ jjb_1.groovy
@@ -7,66 +7,66 @@ import jenkins.model.GlobalConfiguration
 import hudson.tasks.*
 import hudson.util.Secret
 
+def jenkins = Jenkins.instance
+def viewName = '${env}_CS_view'
 
-    def jenkins = Jenkins.instance
-    def viewName = '${env}_CS_view'
-    // Check if the view already exists, and create it if not
-    def existingName = jenkins.getView(viewName)
-    
-    if (existingName == null) {
-        def newView = new ListView(viewName)
-        jenkins.addview(newView)
-        println("View '$viewName' created.")
-        } else {
-        println("View '$viewName' already exists.")
+// Check if the view already exists, and create it if not
+def existingView = jenkins.getView(viewName)
+
+if (existingView == null) {
+    def newView = new ListView(viewName)
+    jenkins.addView(newView) // Corrected method name here
+    println("View '$viewName' created.")
+} else {
+    println("View '$viewName' already exists.")
+}
+
+matrixJob('${env}_CS_job') {
+    description('This is an 1st cc Job DSL job')
+
+    configure { project ->
+        project / 'logRotator' {
+            numToKeep(4) // Corrected method name here
+        }
     }
 
-    matrixJob('${env}_CS_job') {
-    description('This is an 1st cc Job DSL job')
-    
-    configure { project ->
-         project / 'logRotator' {
-            numtokeep(4)
-                                }
-              }
-              
     concurrentBuild(true)
-    
+
     scm {
-      none()
-        }
-        
+        none()
+    }
+
     parameters {
-        stringParam('custName', 'NONE','description')
-               }
-               
+        stringParam('custName', 'NONE', 'description')
+    }
+
     axes {
-           axis {
-                  name ('dynaxis')
-                  valueString(DYNAMIC)
-                }
-         }
-         
-    def globalConfiguration = GlobalConfiguration.all().find {it.displayName == 'Mask Passwords and Regexes' }
-    
+        axis {
+            name('dynaxis')
+            valueString(DYNAMIC)
+        }
+    }
+
+    def globalConfiguration = GlobalConfiguration.all().find { it.displayName == 'Mask Passwords and Regexes' }
+
     if (globalConfiguration) {
-          globalConfiguration.setMaskPasswords(true)
-          globalConfiguration.setMaskPasswordsConsoleLog(true)
-          globalConfiguration.save()
+        globalConfiguration.setMaskPasswords(true)
+        globalConfiguration.setMaskPasswordsConsoleLog(true)
+        globalConfiguration.save()
 
-          System.setProperty("org.jenkinsci.plugins.workflow.steps.SecretEnvVarHelper.CONFIGURED", "true")
-          
-          println("Enabled 'Mask passwords and regexes' feature and global passwords.")
-          } else {
-          println("'Mask passwords and regexes' feature and global passwords not found.")
-          }
-    
+        System.setProperty("org.jenkinsci.plugins.workflow.steps.SecretEnvVarHelper.CONFIGURED", "true")
+
+        println("Enabled 'Mask passwords and regexes' feature and global passwords.")
+    } else {
+        println("'Mask passwords and regexes' feature and global passwords not found.")
+    }
+
     steps {
-      shell('echo ""')
-          }
-          
-myView = hudson.model.Hudson.instance.getView('${env}_CS_view')
-myView.doAddJobToView('${env}_CS_job')
-jenkins.save()
+        shell('echo ""')
+    }
 
+    myView = hudson.model.Hudson.instance.getView('${env}_CS_view')
+    myView.doAddJobToView('${env}_CS_job')
+    jenkins.save()
 }
+
